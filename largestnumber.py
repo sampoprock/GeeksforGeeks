47d7753935dfac45ef164c9aefe2338db8147fcc
Samanway <43331341+sampoprock@users.noreply.github.com>
2020-09-21 21:09:18 +0530

    Add files via upload

---
+++ largestnumber.py
@@ -0,0 +1,27 @@
+# Largest Number
+# Asked in:  
+# Amazon
+# Goldman Sachs
+# Microsoft
+# Given a list of non negative integers, arrange them such that they form the largest number.
+
+# For example:
+
+# Given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+
+# Note: The result may be very large, so you need to return a string instead of an integer.
+
+
+
+
+
+class LargerNumKey(str):
+    def __lt__(x, y):
+        return x+y > y+x
+
+class Solution:
+    # @param A : tuple of integers
+    # @return a strings
+    def largestNumber(self, A):
+        largest_num = ''.join(sorted(map(str, A), key=LargerNumKey))
+        return '0' if largest_num[0] == '0' else largest_num
